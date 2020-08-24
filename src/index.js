7c08971988d161055e28ff52268827300fa719e4
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-24 19:23:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+let addToy2 = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainer = document.querySelector(".container");
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
+    addToy = !addToy;
+    if (addToy) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
